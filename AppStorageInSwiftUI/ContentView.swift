1660f2bfa20672b0bac4cba06bc6e88e82b8a57f
Renato Mateus <renatomateusx@gmail.com>
2021-02-09 21:11:24 -0300

    AppStorage Initial Commit

--- AppStorageInSwiftUI/ContentView.swift
+++ AppStorageInSwiftUI/ContentView.swift
@@ -8,73 +8,43 @@
 import SwiftUI
 import CoreData
 
-struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
-    var body: some View {
-        List {
-            ForEach(items) { item in
-                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-            }
-            .onDelete(perform: deleteItems)
-        }
-        .toolbar {
-            #if os(iOS)
-            EditButton()
-            #endif
-
-            Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")
-            }
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
+struct Settings {
+    static let firstNameKey = "firstName"
+    static let lastNameKey = "lastName"
+    static let isSubscribedKey = "isSubscribed"
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
+struct ContentView: View {
+    
+    @AppStorage(Settings.firstNameKey) var firstName = ""
+    @AppStorage(Settings.lastNameKey) var lastName = ""
+    @AppStorage(Settings.isSubscribedKey) var isSubscribed = false
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                Form {
+                    Section(header: Text("Details")){
+                        TextField("First Name", text: $firstName)
+                            
+                        TextField("Last Name", text: $lastName)
+                    }
+                    Section(header: Text("Member Status")){
+                        Toggle("Is Subscribed", isOn: $isSubscribed)
+                    }
+                }
+            }
+            .navigationTitle("AppStorage Example")
+        }
+    }
+
+   
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
+            .previewDevice("iPhone 12")
     }
 }
